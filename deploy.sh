d4f9f4dc2c6f85f7b99d1225690c177998a01708
Steven Garcia <Garciaintech@gmail.com>
2022-04-25 16:09:35 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,8 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# if you are deploying to https://platacoder.github.io
+git push -f git@github.com:platacoder/platacoder.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
